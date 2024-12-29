215ea47acd075e4ab42d0445cfb5eaf469298cd6
10_days_till_xmas <159474213+10-days-till-xmas@users.noreply.github.com>
2024-12-29 05:38:20 +0000

    Prevent score submissions just in case

--- RocketPatcher/Plugin.cs
+++ RocketPatcher/Plugin.cs
@@ -13,8 +13,20 @@ namespace RocketPatcher
             Logger = base.Logger;
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
             Harmony.CreateAndPatchAll(typeof(GrenadePatcher), PluginInfo.PLUGIN_GUID);
+            Harmony.CreateAndPatchAll(typeof(ScoreSubmissionPatcher), PluginInfo.PLUGIN_GUID);
             Logger.LogMessage("I HATE ZOOMIES!!! I HATE ZOOMIES!!! I HATE ZOOMIES!!!");
             Logger.LogMessage("ExecuteAction(delegate(){grim.Explode();});");
+        }
+
+        private sealed class ScoreSubmissionPatcher
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(GameStateManager), "CanSubmitScores", MethodType.Getter)]
+            static void ScoresSubmission(ref bool __result)
+            {
+                // prevent scores from being submitted so making a universal clip accidentally doesn't cause havoc
+                __result = false;
+            }
         }
     }
 }
