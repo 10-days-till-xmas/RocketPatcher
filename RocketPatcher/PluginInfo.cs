d85b3e1f3ccade0e58310c0b5f34ec1e0a1f8091
10_days_till_xmas <159474213+10-days-till-xmas@users.noreply.github.com>
2024-12-29 05:24:51 +0000

    update version

--- RocketPatcher/PluginInfo.cs
+++ RocketPatcher/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "com.10-days-till-xmas.rocketpatch";
         public const string PLUGIN_NAME = "RocketPatcher";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
